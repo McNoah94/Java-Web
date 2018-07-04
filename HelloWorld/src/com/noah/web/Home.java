bec8020c9e451e546f52732660b98ad62f948a47
Noah <mcnoah94@gmail.com>
2018-07-03 22:53:44 -0400

    first commit

---
+++ HelloWorld/src/com/noah/web/Home.java
@@ -0,0 +1,61 @@
+package com.noah.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Home
+ */
+@WebServlet("/Home")
+public class Home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Home() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String fname = request.getParameter("fname");
+		String lname = request.getParameter("lname");
+		String lang = request.getParameter("lang");
+		String hometown = request.getParameter("hometown");
+		
+		if(fname == null)
+			fname = "unknown";
+		if(lname == null)
+			lname = "unknown";
+		if(lang == null)
+			lang = "unknown";
+		if(hometown == null)
+			hometown = "unknown";
+		
+		out.write("<h1>Hello," + fname + " " + lname + "</h1>" );
+		out.write("<h3>Your favorite language is " + lang + "</h3>");
+		out.write("<h3>Your hometown is " + hometown + "</h3>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
